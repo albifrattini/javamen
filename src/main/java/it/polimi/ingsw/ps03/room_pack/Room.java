c9bfeea9e682333f22f83fd3f24d512eaadc5a05
gabri5991 <gabriele.ghiringhelli@mail.polimi.it>
2017-05-16 14:09:26 +0200

    prova

--- src/main/java/it/polimi/ingsw/ps03/room_pack/Room.java
+++ src/main/java/it/polimi/ingsw/ps03/room_pack/Room.java
@@ -1,14 +1,7 @@
 package it.polimi.ingsw.ps03.room_pack;
 
-public class Room {
+public abstract class Room {
 
 	private int requirementRoom;
 	
-	public Room(int value){
-		this.requirementRoom = value;
-	}
-	
-	public int getRequirementRoom(){
-		return this.requirementRoom;
-	}
 }
