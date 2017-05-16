a93c0980f42bfabdc46622a0d842a61a039efae3
gabri5991 <gabriele.ghiringhelli@mail.polimi.it>
2017-05-16 12:00:52 +0200

    classe room astratta

---
+++ src/main/java/it/polimi/ingsw/ps03/room_pack/Room.java
@@ -0,0 +1,14 @@
+package it.polimi.ingsw.ps03.room_pack;
+
+public class Room {
+
+	private int requirementRoom;
+	
+	public Room(int value){
+		this.requirementRoom = value;
+	}
+	
+	public int getRequirementRoom(){
+		return this.requirementRoom;
+	}
+}
