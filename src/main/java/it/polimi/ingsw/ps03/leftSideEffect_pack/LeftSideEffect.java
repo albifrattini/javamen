844f5704db36f8aa15b16229fd743f3b13b28084
ehre95 <francesco.ehrenheim@mail.polimi.it>
2017-05-20 14:43:34 +0200

    leader and LSE

---
+++ src/main/java/it/polimi/ingsw/ps03/leftSideEffect_pack/LeftSideEffect.java
@@ -0,0 +1,12 @@
+package it.polimi.ingsw.ps03.leftSideEffect_pack;
+/*
+public class LeftSideEffect {
+	private ProductionLSE productionLSE;
+	private HarvestingLSE harvestingLSE;
+	
+	public LeftSideEffect() {
+		productionLSE = new ProductionLSE();
+		harvestingLSE = new HarvestingLSE();
+		}
+		/*
+}
