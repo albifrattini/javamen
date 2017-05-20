79a29fc6211aa2b3b2ac94c282e7057504a208bb
albifrattini <alberto.frattini@mail.polimi.it>
2017-05-20 18:35:50 +0200

    customize project

--- src/main/java/it/polimi/ingsw/ps03/room_pack/Table.java
+++ src/main/java/it/polimi/ingsw/ps03/room_pack/Table.java
@@ -11,6 +11,9 @@ import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
+import it.polimi.ingsw.ps03.points_pack.*;
+import it.polimi.ingsw.ps03.resource_pack.*;
+
 //import it.polimi.ingsw.ps03.resource_pack.Bonus;
 
 public class Table {
@@ -46,73 +49,61 @@ public class Table {
 	
 	public void buildTowerRooms(){
 		
+		int coins = 0;
+		int woods = 0;
+		int stones = 0;
+		int servants = 0;
+		int militaryPoints = 0;
+		TowerColor color = TowerColor.GREEN;
+		int requirement = 0; 
+		
 		try{
 			
-			//apro il file in un oggetto di tipo file per fare operazioni su di esso
 			File towerRoomsXml = new File("/Users/alberto/prova-finale-template/src/main/java/it/polimi/ingsw/ps03/"
-					+ "tower_rooms_arraylist_2.xml"); 
-			/*definisce una api factory che permette all'applicazione di ottenere un parser
-			 *che produce alberi oggetti da un documento xml
-			 *uso .newInstance() perchè il costruttore è protected
-			 */
+					+ "tower_rooms_arraylist_2.xml");
 			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-			/*crea un nuovo oggetto di tipo DocumentBuilder
-			 *una volta che è stato istanziato, xml può essere parsato
-			 *in modi diversi di input
-			 */
 			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-			/*parsa il contenuto del file come un documento xml e ritorna 
-			 *un nuovo oggetto DOM Document
-			 */
 			Document docTowerRooms = dBuilder.parse(towerRoomsXml);
-			
+		
 			docTowerRooms.getDocumentElement().normalize();
-			
-			//printa a video il "titolo"
-			//System.out.println("Root Element: " + docTowerRooms.getDocumentElement().getNodeName());
-			//crea una lista di nodi che hanno come iniziale "towerRoom"
+			docTowerRooms.getDocumentElement().getNodeName();
+
 			NodeList towerRoomList = docTowerRooms.getElementsByTagName("towerRoom");
-			
-			
+		
 			for(int i = 0; i < towerRoomList.getLength(); i++){
-				
 				Node towerRoom = towerRoomList.item(i);
 				
 				if(towerRoom.getNodeType() == Node.ELEMENT_NODE){
+					Element eElement = (Element) towerRoom;
+					String colore = (String) eElement.getElementsByTagName("color").item(0).getTextContent().toUpperCase();
+					color = TowerColor.valueOf(colore);
+					String require = (String) eElement.getElementsByTagName("requirement").item(0).getTextContent();
+					requirement = Integer.parseInt(require);
+					String coin = (String) eElement.getElementsByTagName("coins").item(0).getTextContent();
+					coins = Integer.parseInt(coin);
+					String wood = (String) eElement.getElementsByTagName("woods").item(0).getTextContent();
+					woods = Integer.parseInt(wood);
+					String stone = (String) eElement.getElementsByTagName("stones").item(0).getTextContent();
+					stones = Integer.parseInt(stone);
+					String servant = (String) eElement.getElementsByTagName("servants").item(0).getTextContent();
+					servants = Integer.parseInt(servant);
+					String militaryPoint = (String) eElement.getElementsByTagName("militaryPoints").item(0).getTextContent();
+					militaryPoints = Integer.parseInt(militaryPoint);
+				}
 				
-				
-				Element eElement = (Element) towerRoom;
-				String colore = (String) eElement.getElementsByTagName("color").item(0).getTextContent().toUpperCase();
-				TowerColor color = TowerColor.valueOf(colore);
-				String require = (String) eElement.getElementsByTagName("requirement").item(0).getTextContent();
-				int requirement = Integer.parseInt(require);
-				String givenBonus = (String) eElement.getElementsByTagName("givenBonus").item(0).getTextContent().toUpperCase();
-				
-				
-				TowerRoom nTRoom = new TowerRoom(color, requirement, givenBonus);
+				Resource resource = new Resource(
+						new Coins(coins), new Woods(woods),
+						new Stones(stones), new Servants(servants));
+				MilitaryPoints mPoints = new MilitaryPoints(militaryPoints);
+				Bonus gBonus = new Bonus(resource, mPoints);
+				TowerRoom nTRoom = new TowerRoom(color, requirement, gBonus);
 				towerRooms.add(i, nTRoom);
-
-				}
 			}
-			
-			}catch(Exception e){
-				e.printStackTrace();
-			}
-			
-			
-			try {
-				
-				
-				for (int i = 0; i < 16; i++) {
-					if(towerRooms.get(i) == null) throw new IndexOutOfBoundsException("Valori terminati");
-				
-					System.out.println("Color: " + towerRooms.get(i).getColor());
-					System.out.println("Requirement: " + towerRooms.get(i).getRequirement());
-				}
-					
-			} catch (IndexOutOfBoundsException e){
-				e.getMessage();
-			}
+		
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		
 	}
 	
 
